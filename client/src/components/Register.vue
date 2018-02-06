5d90b552f362910132715a05b604721165631d61
Garret Doe <garrethimself@gmail.com>
2018-02-06 10:53:52 -0500

    vuetify

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,14 +1,22 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input v-model="email" type="email" name="email" placeholder="email"/>
-    <br>
-    <input v-model="password" type="password" name="password" placeholder="password"/>
-    <br>
-    <div class="error" v-html="error"/>
-    <br>
-    <button @click="register">Register</button>
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+        <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+          <div class="pl-4 pr-4 pt-2 pb-2">
+            <input v-model="email" type="email" name="email" placeholder="email"/>
+            <br>
+            <input v-model="password" type="password" name="password" placeholder="password"/>
+            <br>
+            <div class="error" v-html="error"/>
+            <br>
+            <v-btn class="cyan" @click="register">Register</v-btn>
+          </div>
+        </div>
+      </v-flex>
+  </v-layout>
 </template>
 
 <script>
