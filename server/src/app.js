8ab3a204adb5c31cd9a3814fcc063689490be41c
Garret Doe <garrethimself@gmail.com>
2018-02-05 19:09:16 -0500

    simple front/back end communication via register method

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-//const cors = require('cors');
+const cors = require('cors');
 const morgan = require('morgan');
 
 const app = express();
-app.use(morgan('combine'));
+app.use(morgan());
 app.use(bodyParser.json());
-//app.use(cors());
+app.use(cors());
 
 app.get('/status', (req, res) => {
     res.send({
         message: "hello world!"
+    });
+});
+
+app.post('/register', (req, res) => {
+    res.send({
+        message: `Hello ${req.body.email}!, Your user was registered. Have fun!`,
     });
 });
 
